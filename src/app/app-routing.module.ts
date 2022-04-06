1168420aad6f8c2e908f033102b9071c9dc6dab3
Ismaël Meziani <meziani-i@outlook.fr>
2022-04-06 23:28:33 +0200

    simple layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: TutorialComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'tutorial', component: TutorialComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    TutorialComponent,
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
